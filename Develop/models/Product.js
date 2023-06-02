eeb1eddec7f889e40a7fef8bc626508a63703397
mahdi83777 <mye09@mail.aub.edu>
2023-06-02 18:59:45 +0300

    updated product.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,39 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    // product has id as primary, product_name, price, stock and category id which references id in category
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
